b8c2acb68dad4249ed9c56d94cfba14fe97b7f92
Sfever <136332266+Sfever@users.noreply.github.com>
2024-03-02 10:58:17 +0800

    Adding functions to mainmenu.py and init.py

--- codes/mainmenu.py
+++ codes/mainmenu.py
@@ -1,2 +1,11 @@
-import init
-init.it()
+import init as base
+import pygame as pg
+import pygame.locals as pgl 
+main=base.inital()
+mainwindow=main.get_screen_object()
+while True:
+    for event in pg.event.get():
+        if event.type == pgl.QUIT:
+            main.onquit()
+
+
