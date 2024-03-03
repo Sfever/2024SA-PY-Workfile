eef0f5570a789d163c00349f76627c39d5a94ad7
Sfever <136332266+Sfever@users.noreply.github.com>
2024-03-03 21:19:11 +0800

    textview updated, change the implentation of texts in main.py

--- codes/textview.py
+++ codes/textview.py
@@ -6,6 +6,11 @@ Please do not use your own implentation
 import init as base
 import pygame as pg
 class textobj:
+    text=""
+    surface=""
+    color=[]
+    font=""
+    title=""
     def __init__(self):
         self.text=""
         self.surface=""
@@ -24,18 +29,20 @@ class textobj:
     def set_text(self):
         self.text=pg.font.SysFont(self.font,self.size)
     def render_surface(self):
-        self.surface=self.text.render(self.title,True,self.color)
+        self.surface=self.text.render(self.title,True,tuple(self.color))
     def get_text(self):
-        return self.text
+        return self.text#returns the object of font
     def get_surface(self):
-        return self.surface
+        return self.surface#returns rendered surface
     def get_dimensions(self):
-        return self.text.size()
+        return self.text.size(self.title)
+    #make surface invisible
     def delete_text(self):
         self.surface.set_alpha(0)
         pg.display.update()
 
 class textview:
+    textlist=[]
     def __init__(self):
         self.textlist=[]
     def new_text(self):
@@ -44,7 +51,10 @@ class textview:
         return len(self.textlist)-1
     def del_text(self,tgt):
         self.textlist[tgt].delete_text()
-        del self.textlist[tgt]
+        del self.textlist[tgt]#remove it and make become not accessible anymore
     '''
     self.textlist will not be provided outside the class in order to prevent losting access to textobj object accidentally
-    ''' 
+    '''
+    #returns the instance of textobj for devs to work on
+    def get_object(self,index):
+        return self.textlist[index]
