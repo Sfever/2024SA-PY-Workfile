8cec543df0edc0e9cd7e8cbb4821d5c650aea4ec
Sfever <136332266+Sfever@users.noreply.github.com>
2024-03-04 21:47:05 +0800

    disable render in tests

--- main_test.py
+++ main_test.py
@@ -31,7 +31,6 @@ font4title.set_text()
 
     #show background if it's ther
 
-font4title.render_surface()
     #get title text size for show it in center
 F_width,F_height=font4title.get_dimensions()
     #show title text
