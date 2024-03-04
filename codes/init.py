ccaa95453afd25ac364d1d8344a7631a37b1c76f
Sfever <136332266+Sfever@users.noreply.github.com>
2024-03-04 20:12:38 +0800

    several not important update, increase readablity

--- codes/init.py
+++ codes/init.py
@@ -22,6 +22,7 @@ class inital:
         if os.path.exists(self.config_path):
             if os.access(self.config_path,os.R_OK):
                 self.config_exist=1
+                
         if self.config_exist==0:
             warnings.warn("config.json can not be read,loading defaults...")
             for i in tr.trange(100):
@@ -35,15 +36,18 @@ class inital:
             config={}
             with open(self.config_path,'r') as config_reader:#read configs
                 config=json.load(config_reader)
+
             jump_bar=config["jumpbar"]
             if jump_bar!=True:
                 for i in tr.trange(100):
                     time.sleep(round(random.uniform(0.0001,0.2),5))#absolutely weird, this progress bar indicates nothing
             time.sleep(1)
+
             print("config.json loaded as",config)
             self.W_width=config["resolution_horizontal"]
             self.W_height=config["resolution_vertical"]#load configs
             self.caption=config["caption"]
+
         self.screen=pg.display.set_mode([self.W_width,self.W_height])
         pg.display.set_caption(self.caption)#initializing window
         end_time=time.time()
