31ecc1f3b5e0c94a2306ff003020587c605a89dc
Sfever <136332266+Sfever@users.noreply.github.com>
2024-03-04 21:38:55 +0800

    test

--- main_test.py
+++ main_test.py
@@ -28,20 +28,12 @@ if main.config_exist==1:
 
 font4title.set_text()
 
-while True:
-    for event in pg.event.get():
-        if event.type == pgl.QUIT:
-            main.onquit()
 
-    #show background if it's there
-    if bgexist==1:
-        mainwindow.blit(bg,(0,0))
+    #show background if it's ther
 
-    font4title.render_surface()
+font4title.render_surface()
     #get title text size for show it in center
-    F_width,F_height=font4title.get_dimensions()
+F_width,F_height=font4title.get_dimensions()
     #show title text
-    mainwindow.blit(font4title.get_surface(),((main.get_reso_horizontal()-F_width)/2,100))
     #Update display
-    pg.display.update()
-    main.onquit()
+main.onquit()
