14f679b73344f4cf0bf178e62a28b0e872fe0cda
codybryant114 <161816113+codybryant114@users.noreply.github.com>
2024-03-20 17:31:55 +0800

    ？？？

--- codes/textview.py
+++ codes/textview.py
@@ -4,30 +4,30 @@ All texts should use this file
 Please do not use your own implentation
 '''
 #typehints
-import pygame as pg
-class textobj:
-    text=""
-    surface=""
-    color=[]
-    font=""
-    title=""
-    def __init__(self):
+import pygame as pg #导入pygame储存为pg库方便后续调用
+class textobj:  #类textobj方便后续调用
+    text=""     #定义文本类
+    surface=""  #定义文本类
+    color=[]    #定义文本类
+    font=""     #定义文本类
+    title=""    #定义文本类    （选择变量类型）
+    def __init__(self):#初始化
         self.text=""
         self.surface=""
-        self.color=[255,255,255]
-        self.size=30
-        self.font="arial"
-        self.title="Fuck this world"
-    def set_color(self,color:list):
+        self.color=[255,255,255] #设置初始颜色？
+        self.size=30             #设置初始大小？
+        self.font="arial"        #设置初始字体？
+        self.title="Fuck this world" #设置标题
+    def set_color(self,color:list):  #设置颜色清单？  （这一块在创建变量？）
         self.color=color
-    def set_font(self, font:str):
+    def set_font(self, font:str):    
         self.font=font
     def set_size(self,size:int):
         self.size=size
     def set_content(self,content:str):
         self.title=content
     def set_text(self):
-        self.text=pg.font.SysFont(self.font,self.size)
+        self.text=pg.font.SysFont(self.font,self.size)  #这一块才是设置变量的值？
     def render_surface(self):
         self.surface=self.text.render(self.title,True,tuple(self.color))
     def get_text(self):
@@ -39,16 +39,16 @@ class textobj:
     def delete_text(self):
         self.surface.set_alpha(0)#make surface invisible
         pg.display.update()
-        print("Successed to remove text")
+        print("Successed to remove text") #设置完了print出来？
 
 class textview:
-    def __init__(self):
+    def __init__(self): #初始化
         self.textlist=[]
-    def new_text(self):
+    def new_text(self):  #新建文本对象？（什么ppt操作）
         newtext=textobj()
         self.textlist.append(newtext)
         return len(self.textlist)-1
-    def del_text(self,tgt):
+    def del_text(self,tgt):    #删除文本对象？
         self.textlist[tgt].delete_text()
         del self.textlist[tgt]#remove it and make become not accessible anymore
         #print("Successed to remove text")
