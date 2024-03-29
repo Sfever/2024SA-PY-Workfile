9bd1517f81f604bd7e12684a65131c3eba21449b
codybryant114 <161816113+codybryant114@users.noreply.github.com>
2024-03-29 22:11:47 +0800

    Lambert's 迷之注释

--- codes/textview.py
+++ codes/textview.py
@@ -4,16 +4,16 @@ All texts should use this file
 Please do not use your own implentation
 '''
 #typehints
-import pygame as pg
-class textobj:
-    def __init__(self):
-        self.text=""
-        self.surface=""
-        self.color=[255,255,255]
-        self.size=30
-        self.font="arial"
-        self.title="Fuck this world"
-    def set_color(self,color:list):
+import pygame as pg  #Import pygame and store it as a pg library for subsequent calls.
+class textobj:       #The textobj class facilitates subsequent calls
+    def __init__(self):#initialisation
+        self.text=""     
+        self.surface=""   
+        self.color=[255,255,255]  #Setting the initial colour
+        self.size=30    #Setting the initial size
+        self.font="arial"  #Setting the initial font
+        self.title="Fuck this world"   #setting the title
+    def set_color(self,color:list): #Setting up a list of colours (creating groups of variables)
         self.color=color
     def set_font(self, font:str):
         self.font=font
@@ -22,7 +22,7 @@ class textobj:
     def set_content(self,content:str):
         self.title=content
     def set_text(self):
-        self.text=pg.font.SysFont(self.font,self.size)
+        self.text=pg.font.SysFont(self.font,self.size) #Setting the value of a variable
     def render_surface(self):
         self.surface=self.text.render(self.title,True,tuple(self.color))
     def get_text(self):
@@ -34,16 +34,16 @@ class textobj:
     def delete_text(self):
         self.surface.set_alpha(0)#make surface invisible
         pg.display.update()
-        print("Successed to remove text")
+        print("Successed to remove text") #print it out at the last
 
 class textview:
-    def __init__(self):
+    def __init__(self): #initialisation
         self.textlist=[]
-    def new_text(self):
+    def new_text(self):  #Creating Text Object
         newtext=textobj()
         self.textlist.append(newtext)
         return len(self.textlist)-1
-    def del_text(self,tgt):
+    def del_text(self,tgt): #Delete thw Text Object
         self.textlist[tgt].delete_text()
         del self.textlist[tgt]#remove it and make become not accessible anymore
         #print("Successed to remove text")
